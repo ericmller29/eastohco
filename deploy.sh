95f1db496b9004f4b7f9ae8129854e663ff02a1a
Eric <emiller@tax-guard.com>
2016-01-05 07:58:20 -0700

    Deploying -

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 ###############################################################################
-echo $(date)
+echo $(date)+'%Y-%m-%d'
 set -e
 GITURL=`git config remote.origin.url`
 git add --all
